4264989a4173f56dddcf6a81fe75090c3c015c17
Muhammad Aneeq <muhammadaneeqsellharborx@gmail.com>
2025-11-12 03:24:46 +0500

    Fix bcrypt password truncation for hashing

--- app/utils.py
+++ app/utils.py
@@ -29,8 +29,9 @@ ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
 
 def get_password_hash(password: str) -> str:
-    truncated = password.encode("utf-8")[:72]
-    return pwd_context.hash(password)
+    truncated_password = password.encode("utf-8")[:72].decode("utf-8", errors="ignore")
+    hashed_pw = pwd_context.hash(truncated_password)
+    return hashed_pw
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
